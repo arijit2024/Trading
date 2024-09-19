bc17aeca91951d4ae99af0b8871993e624e813b3
arijit <arijitsarkar2230@gmal.com>
2024-09-19 23:24:05 +0530

    Login api update

--- back-end/server.php
+++ back-end/server.php
@@ -7,7 +7,6 @@ include 'data-conection.php';
 $method = isset($_POST['method']) ? $_POST['method'] : '';
 
 if ($method === "ourUser") {
-
     $user = isset($_POST['user']) ? $_POST['user'] : '';
     $password = isset($_POST['password']) ? $_POST['password'] : '';
     $sql = mysqli_query($con, "SELECT * FROM user WHERE userid='$user'AND password='$password'");
@@ -17,8 +16,18 @@ if ($method === "ourUser") {
     }else{
         echo "No login";
     }
- 
-} else {
+} else if($method === "register"){
+    $numberUser = isset($_POST['numberUser']) ? $_POST['numberUser'] : '';
+    $passUser = isset($_POST['passUser']) ? $_POST['passUser'] : '';
+
+    $sql = mysqli_query($con, "INSERT INTO user (userid, password) VALUES ('$numberUser','$passUser')");
+    if($sql){
+        echo "Succcess";
+    }else{
+        echo "Not Succcess";
+    }
+} 
+else {
     echo "Invalid method";
 }
 ?>
